231117d018f0955db78360d673beee8e22900b5e
M-ASHaR <itisashar3007@gmail.com>
2022-01-29 22:25:52 +0500

    It's a React Todo App to configure daily life Todos...

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { useEffect, useState } from "react";
+import TodoList from "./TodoList";
+
+//to get the data from Local Storage
+const getLocalItems =()=>{
+  let lists=localStorage.getItem('lists');
+  console.log(lists)
+
+  if(lists){
+    return JSON.parse([localStorage.getItem('lists')]);
+  } else{
+    return [];
+  }
 }
 
-export default App;
+const App = () =>{
+  const [inputList,setInputList]=useState("");
+
+
+  const[Items,setItems]= useState([getLocalItems()]);
+ 
+  const itemEvent =(event) =>{
+    setInputList(event.target.value);
+  };
+
+  const ListofItems =() =>{
+  setItems((oldItems) =>{
+    return[...oldItems,inputList];
+  })
+  setInputList("");
+  };
+  const deleteItems=(id) =>{
+    console.log("deleted");
+
+    setItems((oldItems)=>{
+      return oldItems.filter((arrElement,index)=>{
+        return index !==id;
+
+      })
+    })
+};
+  // add data to local storage 
+  useEffect(() =>{
+    localStorage.setItem('lists',JSON.stringify(Items))
+  }, [Items]); 
+
+    return <>
+    <div className="main_div">
+        <div className="center_div">
+            <br />
+            <h1>TODO List</h1>
+            <br />
+            <input type="text" placeholder="Add Items" value={inputList}
+            onChange={itemEvent}/>
+            <button onClick={ListofItems}> + </button> 
+
+            <ol>
+                {/* <li> {inputList}</li> */}
+
+                {Items.map((itemval, index)=>{
+                 return <TodoList 
+                 key={index} 
+                 id={index}
+                 text={itemval} 
+                onSelect={deleteItems}/>;
+                })}
+            </ol>
+            </div>        
+ 
+
+    </div>
+    
+    </>
+
+}
+ export default App;
